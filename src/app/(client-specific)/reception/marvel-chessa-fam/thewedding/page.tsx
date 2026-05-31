9731a80c1337547bfca3b495f82733a9c3f2e4f2
Ricky Jonathan <64757579+jonathanricky@users.noreply.github.com>
2026-05-31 16:57:53 +0200

    Update tea ceremony time from '2.30 pm' to '2 pm'

--- src/app/(client-specific)/reception/marvel-chessa-fam/thewedding/page.tsx
+++ src/app/(client-specific)/reception/marvel-chessa-fam/thewedding/page.tsx
@@ -150,7 +150,7 @@ export default function Thewedding() {
             TEA CEREMONY
           </h2>
           <h3 className="mb-[6px] font-freight text-[16px] tracking-wide drop-shadow-2xl lg:text-[18px] 2xl:text-[20px]">
-            2.30 pm — WITA
+            2 pm — WITA
           </h3>
           <h4 className="mb-[6px] font-hanken text-[12px] font-light drop-shadow-2xl lg:text-[14px] 2xl:text-[16px]">
             Venue TBC
